661c7a667852269a9077003b8b053f43ce480a94
Abs kawser <abskawser77000@gmail.com>
2023-02-14 00:56:52 +0600

    pacakge include

---
+++ src/app.js
@@ -0,0 +1,5 @@
+// Basic Lib Import
+const express =require('express');
+const router =require('./src/routes/api');
+const app= new express();
+const bodyParser =require('body-parser');
---
+++ src/routes/api.js
@@ -0,0 +1,22 @@
+// Basic Lib Import
+const express =require('express');
+const router =require('./src/routes/api');
+const app= new express();
+const bodyParser =require('body-parser');
+
+// Security Middleware Lib Import
+const rateLimit =require('express-rate-limit');
+const helmet =require('helmet');
+const mongoSanitize =require('express-mongo-sanitize');
+const xss =require('xss-clean');
+const hpp =require('hpp');
+const cors =require('cors');
+
+const mongoose =require('mongoose');
+
+// Body Parser Implement
+app.use(bodyParser.json())
+
+// Request Rate Limit
+const limiter= rateLimit({windowMs:15*60*1000,max:3000})
+app.use(limiter)
