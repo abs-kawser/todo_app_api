479d7f4598f181f35e156fd9a9578c7fd6d8e2bf
abs-kawser <abskawser77000@gmail.com>
2023-02-14 11:47:14 +0600

    second add

---
+++ src/app.js
@@ -0,0 +1,47 @@
+// Basic Lib Import
+const express =require('express');
+const router =require('./src/routes/api');
+const app= new express();
+const bodyParser =require('body-parser');
+
+// Security Middleware Lib Import
+const rateLimit =require('express-rate-limit');
+const helmet =require('helmet');
+const mongoSanitize =require('express-mongo-sanitize');
+const xss =require('xss-clean');
+const hpp =require('hpp');
+const cors =require('cors');
+
+const mongoose =require('mongoose');
+
+// Body Parser Implement
+app.use(bodyParser.json())
+
+// Request Rate Limit
+const limiter= rateLimit({windowMs:15*60*1000,max:3000})
+app.use(limiter)
+
+
+
+
+// Mongo DB Database Connection
+let URI="mongodb+srv://kawser:kawser@cluster0.refdpyx.mongodb.net/?retryWrites=true&w=majority";
+let OPTION={autoIndex:true}
+mongoose.connect(URI,OPTION,(error)=>{
+    console.log("Connection Success")
+    console.log(error)
+})
+
+// Routing Implement
+app.use("/api/v1",router)
+
+
+// Undefined Route Implement
+app.use("*",(req,res)=>{
+    res.status(404).json({status:"fail",data:"Not Found"})
+})
+
+
+
+
+module.exports=app;
